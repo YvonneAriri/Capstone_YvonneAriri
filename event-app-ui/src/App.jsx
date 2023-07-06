e1919ad7792a2bcc17b3928111ecb671c69d9a61
Yvonne Ariri <yvonneariri@meta.com>
2023-07-06 11:43:21 -0700

    setting up my project

---
+++ event-app-ui/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button>Log In</button>
+      <button>Sign Up</button>
+    </div>
+  );
+}
+
+export default App;
