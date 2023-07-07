633a50c56d749d0c02f67b27d798e4188eb8dfd7
Yvonne Ariri <yvonneariri@meta.com>
2023-07-07 10:35:29 -0700

    made small changes by adding foreignKey to the event.belongsTo

--- event-app-api/models/index.js
+++ event-app-api/models/index.js
@@ -2,6 +2,6 @@ import { User } from "./user.js";
 import { Event } from "./event.js";
 
 User.hasMany(Event, { foreignKey: "username" });
-Event.belongsTo(User);
+Event.belongsTo(User, { foreignKey: "username" });
 
 export { User, Event };
