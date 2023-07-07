20ae5b120abd17176272227ebd8ae443f80f28b3
Yvonne Ariri <yvonneariri@meta.com>
2023-07-07 08:26:38 -0700

    inserting the values inputed into my database

--- event-app-api/server.js
+++ event-app-api/server.js
@@ -5,9 +5,38 @@ import { sequelize } from "./database.js";
 import { User, Event } from "./models/index.js";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.post("/signUp", (req, res) => {
+  const fullname = req.body.fullname;
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const tel = req.body.tel;
+
+  console.log("!!!", req);
+
+  sequelize.query(
+    `INSERT INTO "public"."Users" (fullname, username, password, email, tel, "createdAt", "updatedAt") VALUES ('${fullname}', '${username}', '${password}', '${email}', '${tel}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`,
+    { type: sequelize.QueryTypes.INSERT },
+    (err, result) => {
+      console.log(err);
+    }
+  );
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 sequelize
