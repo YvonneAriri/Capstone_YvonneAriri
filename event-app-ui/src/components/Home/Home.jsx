844d870a05362292a1b77fedff442df19ecfaed0
Yvonne Ariri <yvonneariri@meta.com>
2023-07-12 11:32:15 -0700

    imported link from react-router-dom to enable the user to go to the login and signup page once the respective button is clicked

--- event-app-ui/src/components/Home/Home.jsx
+++ event-app-ui/src/components/Home/Home.jsx
@@ -1,10 +1,22 @@
 import "components/Home/Home.css";
+import { Link } from "react-router-dom";
 import Navbar from "components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="home">
       <Navbar />
+      <div className="btn">
+        {/* links the button login to the login page */}
+        <Link to={"login"}>
+          <button className="login-btn">Login</button>
+        </Link>
+
+        {/* links the button signup to the signup Page */}
+        <Link to={"signup"}>
+          <button className="signup-btn">SignUp</button>
+        </Link>
+      </div>
     </div>
   );
 }
