958d220b684bd9dbc9402a6689a08e5aa2ddaf54
Yvonne Ariri <yvonneariri@meta.com>
2023-07-17 22:00:34 -0700

    used the disabled attribute to not allow the users to sumbit while the form is empty, and displayed the errorMessage created in the server side

--- event-app-ui/src/components/Login/LoginForm.jsx
+++ event-app-ui/src/components/Login/LoginForm.jsx
@@ -1,34 +1,36 @@
-/* eslint-disable no-unused-vars */
 import "components/Login/LoginForm.css";
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
 
   const navigate = useNavigate();
+  const isDisabled = username === "" || password === "";
 
   // includes cookies and and authentication headers in cross-origin request
   axios.defaults.withCredentials = true;
   //sends the users credentials to the server
   const logIn = (e) => {
-    if (username.length === 0 || password.length === 0) {
-      setError(true);
-    } else {
-      e.preventDefault();
-      axios
-        .post("http://localhost:3000/login", {
-          username: username,
-          password: password,
-        })
-        .then((result) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/login", {
+        username: username,
+        password: password,
+      })
+      .then((result) => {
+        // displays the error made in the serverside
+        if (result.data.errorMessage) {
+          setError(result.data.errorMessage);
+        } else {
           // This redirects the user to his profilepage once the user successfully logs in
+
           navigate(`/profile/${result.data[0].username}`);
-        });
-    }
+        }
+      });
   };
 
   return (
@@ -40,36 +42,31 @@ export default function LoginForm() {
           <input
             type="text"
             id="username"
+            value={username}
             onChange={(e) => {
-              setUsername(e.target.value.trim());
+              setUsername(e.target.value);
             }}
+            required
             placeholder="Username"
           />
         </div>
-        {error && (username.length === 0 || username.trim().length === 0) ? (
-          <label>username cannot be empty</label>
-        ) : (
-          ""
-        )}
-        <div className="input-box">
+
+        <div className="input-box" data-tip="8 characters minimum">
           <input
             type="password"
             id="password"
+            value={password}
             onChange={(e) => {
-              setPassword(e.target.value.trim());
+              setPassword(e.target.value);
             }}
             placeholder="Password"
             required
           />
         </div>
-        {error && (password.length === 0 || password.trim().length === 0) ? (
-          <label>password cannot be empty</label>
-        ) : (
-          ""
-        )}
+        <div className="error">{error}</div>
         <div>
           <br />
-          <button className="btn" onClick={logIn}>
+          <button disabled={isDisabled} className="btn" onClick={logIn}>
             Login
           </button>
           <p>
