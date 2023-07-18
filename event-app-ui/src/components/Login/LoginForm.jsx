a99bd896feafff386d1bde4cbbee723c1b9c9c4b
Yvonne Ariri <yvonneariri@meta.com>
2023-07-17 22:00:34 -0700

    used the disabled attribute to not allow the users to sumbit while the form is empty, and displayed the errorMessage created in the server side

--- event-app-ui/src/components/Login/LoginForm.jsx
+++ event-app-ui/src/components/Login/LoginForm.jsx
@@ -6,28 +6,31 @@ import { Link, useNavigate } from "react-router-dom";
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
 
   const navigate = useNavigate();
+  const isDisabled = username === "" || password === "";
 
   // includes cookies and and authentication headers in cross-origin request
   axios.defaults.withCredentials = true;
   //sends the users credentials to the server
   const logIn = (e) => {
-    if (username.length === 0 || password.length === 0) {
-      setError(true);
-    } else {
-      e.preventDefault();
-      axios
-        .post("http://localhost:3000/login", {
-          username: username,
-          password: password,
-        })
-        .then((result) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/login", {
+        username: username,
+        password: password,
+      })
+      .then((result) => {
+        // displays the error made in the serverside
+        if (result.data.errorMessage) {
+          setError(result.data.errorMessage);
+        } else {
           // This redirects the user to his profilepage once the user successfully logs in
+
           navigate(`/profile/${result.data[0].username}`);
-        });
-    }
+        }
+      });
   };
 
   return (
@@ -36,53 +39,42 @@ export default function LoginForm() {
         <div className="form-content">
           <header>Login</header>
           <form action="#" className="form-button">
-            <div className="input-field">
+            <div className="input-box">
               <input
                 autoComplete="off"
                 type="text"
                 id="username"
+                value={username}
                 onChange={(e) => {
-                  setUsername(e.target.value.trim());
+                  setUsername(e.target.value);
                 }}
-                placeholder="Username"
                 required
+                placeholder="Username"
               />
             </div>
-            {error &&
-            (username.length === 0 || username.trim().length === 0) ? (
-              <label>username cannot be empty</label>
-            ) : (
-              ""
-            )}
-            <div className="input-field">
+
+            <div className="input-box" data-tip="8 characters minimum">
               <input
                 autoComplete="off"
                 type="password"
                 id="password"
+                value={password}
                 onChange={(e) => {
-                  setPassword(e.target.value.trim());
+                  setPassword(e.target.value);
                 }}
                 placeholder="Password"
                 required
               />
             </div>
-            {error &&
-            (password.length === 0 || password.trim().length === 0) ? (
-              <label>password cannot be empty</label>
-            ) : (
-              ""
-            )}
+            <div className="error">{error}</div>
             <div>
-              <div>
-                <button className="input-field" onClick={logIn}>
-                  Login
-                </button>
-              </div>
-              <div>
-                <p className="account-exist">
-                  Don&apos;t have an account? <Link to="/signup">signUp</Link>
-                </p>
-              </div>
+              <br />
+              <button disabled={isDisabled} className="btn" onClick={logIn}>
+                Login
+              </button>
+              <p>
+                Don&apos;t have an account? <Link to="/signup">signUp</Link>
+              </p>
             </div>
           </form>
         </div>
