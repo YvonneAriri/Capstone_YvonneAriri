fd363ac7bca9ea355b591d71f82a23b90f30a4d1
Yvonne Ariri <yvonneariri@meta.com>
2023-07-31 12:55:30 -0700

    Enabled users to update their preferredStartTime and preferredEndTime in their profile

--- event-app-ui/src/components/EditProfile/EditProfile.jsx
+++ event-app-ui/src/components/EditProfile/EditProfile.jsx
@@ -16,6 +16,8 @@ export default function EditProfile(props) {
   const [newFullname, setNewFullname] = useState(fullname);
   const [newEmail, setNewEmail] = useState(email);
   const [newTel, setNewTel] = useState(tel);
+  const [preferredStartTime, setPreferredStartTime] = useState("");
+  const [preferredEndTime, setPreferredEndTime] = useState("");
 
   const editProfile = () => {
     axios.post(`http://localhost:3000/editProfile`, {
@@ -23,10 +25,32 @@ export default function EditProfile(props) {
       fullname: newFullname,
       email: newEmail,
       tel: newTel,
+      preferredstarttime: preferredStartTime,
+      preferredendtime: preferredEndTime,
     });
   };
-  const isDisabled = newFullname === "" || newEmail === "" || newTel === "";
+  const isDisabled =
+    newFullname === "" ||
+    newEmail === "" ||
+    (newTel === "" && preferredStartTime === "" && preferredEndTime === "");
 
+  function timeToEpoch(timeString) {
+    const [hours, minutes, seconds] = timeString.split(":");
+    const dateObj = new Date();
+    dateObj.setUTCHours(parseInt(hours, 10));
+    dateObj.setUTCMinutes(parseInt(minutes, 10));
+    dateObj.setUTCSeconds(seconds ? parseInt(seconds, 10) : 0);
+    return dateObj.getTime() / 1000; // Divide by 1000 to get seconds instead of milliseconds
+  }
+  const handleStartChange = (e) => {
+    setPreferredStartTime(e.target.value.trim());
+  };
+  const handleEndChange = (e) => {
+    setPreferredEndTime(e.target.value.trim());
+  };
+  const timeString = "23:41:00";
+  const epochTime = timeToEpoch(timeString);
+  console.log(epochTime); // Output: 1674465660
   return (
     <>
       <button className="close-btn" onClick={() => setOpenProfile(false)}>
@@ -62,6 +86,25 @@ export default function EditProfile(props) {
             setNewTel(e.target.value.trim());
           }}
         />
+        <label htmlFor="timeInput">Enter Time (HH:mm:ss):</label>
+        <input
+          className="edit-input"
+          type="Time"
+          step="1"
+          value={preferredStartTime}
+          placeholder="minStartTime"
+          onChange={handleStartChange}
+        />
+        <label htmlFor="timeInput">Enter Time (HH:mm:ss):</label>
+        <input
+          className="edit-input"
+          type="Time"
+          step="1"
+          value={preferredEndTime}
+          placeholder="maxEndTime"
+          onChange={handleEndChange}
+        />
+
         <div className="center-btn">
           <button
             className="submit-btn"
